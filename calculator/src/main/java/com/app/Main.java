53ccac69e0307da1b134b2f9681247002c5e3bc9
softwareengineerhub <denis.prokopiuk@yahoo.com>
2022-02-02 15:09:55 +0200

    my-feature-branch-commit

--- calculator/src/main/java/com/app/Main.java
+++ calculator/src/main/java/com/app/Main.java
@@ -3,7 +3,7 @@ package com.app;
 public class Main {
 
     public static void main(String[] args) {
-        //this is code
+        //this is code; example feature
         int t=1;
         int x=2;
         System.out.println("BranchA");
